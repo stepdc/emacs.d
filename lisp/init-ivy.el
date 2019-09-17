bbf415befab8ec8d82c7728fa8941b7efe25724a
stepdc <habwtl@foxmail.com>
2019-09-17 13:23:50 +0800

    remove annoying delay

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -336,7 +336,8 @@ If N is nil, use `ivy-mode' to browse `kill-ring'."
   '(progn
      ;; better performance on everything (especially windows), ivy-0.10.0 required
      ;; @see https://github.com/abo-abo/swiper/issues/1218
-     (setq ivy-dynamic-exhibit-delay-ms 250)
+     ;; slow here
+     ;; (setq ivy-dynamic-exhibit-delay-ms 250)
 
      ;; Press C-p and Enter to select current input as candidate
      ;; https://oremacs.com/2017/11/30/ivy-0.10.0/
