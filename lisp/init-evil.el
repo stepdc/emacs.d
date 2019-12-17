6afa1943192605b6de48f2d6c9da63d66ced5138
stepdc <habwtl@foxmail.com>
2019-12-17 17:36:24 +0800

    refactor

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -303,34 +303,6 @@ If the character before and after CH is space or tab, CH is NOT slash"
 (define-key evil-insert-state-map (kbd "C-x C-p") 'evil-complete-previous-line)
 (define-key evil-insert-state-map (kbd "C-]") 'aya-expand)
 
-;; c-w
-(defun prelude-move-beginning-of-line (arg)
-  "Move point back to indentation of beginning of line.
-
-Move point to the first non-whitespace character on this line.
-If point is already there, move to the beginning of the line.
-Effectively toggle between the first non-whitespace character and
-the beginning of the line.
-
-If ARG is not nil or 1, move forward ARG - 1 lines first. If
-point reaches the beginning or end of the buffer, stop there."
-  (interactive "^p")
-  (setq arg (or arg 1))
-
-  ;; Move lines first
-  (when (/= arg 1)
-    (let ((line-move-visual nil))
-      (forward-line (1- arg))))
-
-  (let ((orig-point (point)))
-    (back-to-indentation)
-    (when (= orig-point (point))
-      (move-beginning-of-line 1))))
-
-;; C-a
-(define-key evil-insert-state-map (kbd "C-a") 'prelude-move-beginning-of-line)
-
-
 (defun my-search-defun-from-pos (search pos)
   (evil-search search t t pos)
   ;; ignore this.f1 = this.fn.bind(this) code
@@ -960,6 +932,33 @@ point reaches the beginning or end of the buffer, stop there."
 (define-key evil-normal-state-map (kbd "K") (lambda () (interactive) (backward-paragraph)))
 (define-key evil-normal-state-map (kbd "J") (lambda () (interactive) (forward-paragraph)))
 (define-key evil-normal-state-map (kbd "C-l") (lambda () (interactive) (recenter-top-bottom) (evil-ex-nohighlight)))
+
+;; c-w
+(defun prelude-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first. If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+;; C-a
+(define-key evil-insert-state-map (kbd "C-a") 'prelude-move-beginning-of-line)
 
 ;; global
 (global-set-key (kbd "C-c r") 'my-counsel-recentf)
